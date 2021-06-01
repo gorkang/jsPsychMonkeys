e14a0094284fa57a0b52d7f056fc15b7ecbe296d
Gorka Navarrete <gorkang@gmail.com>
2021-06-01 10:36:52 +0100

    hacky solution to delay after fullscreen. if in instructions, always forward. Avoid warnings.

--- R/interact_with_element.R
+++ R/interact_with_element.R
@@ -36,13 +36,23 @@ interact_with_element <- function(list_get_elements, DEBUG = FALSE) {
       selected_button_name = list_get_elements$name_buttons$id
       list_get_elements$list_elements[[selected_button_name]]$clickElement()
       
+      # If button is Fullscreen, wait a couple seconds so the interface is responsive
+      if (selected_button_name == "jspsych-fullscreen-btn") Sys.sleep(3)
+      
     } else if (length(list_get_elements$name_buttons$id) > 1) {
       
         # IF WE ARE IN CONSENT and debugging: always start
-        if (DEBUG == TRUE & all(list_get_elements$name_buttons$id == c("start", "end"))) {
+        # if (DEBUG == TRUE & all(list_get_elements$name_buttons$id == c("start", "end"))) {
+        if (all(list_get_elements$name_buttons$id == c("start", "end"))) {
 
-          selected_button_name = c("start", "end")
-          list_get_elements$list_elements[["start"]]$clickElement()
+          selected_button_name = c("start")
+          list_get_elements$list_elements[[selected_button_name]]$clickElement()
+          
+        # If we are in instructions with Back/Forward buttons, always Forward
+        } else if (all(list_get_elements$name_buttons$id == c("jspsych-instructions-back", "jspsych-instructions-next"))) {
+          
+          selected_button_name = c("jspsych-instructions-next")
+          list_get_elements$list_elements[[selected_button_name]]$clickElement()
           
         } else {
           
@@ -60,7 +70,7 @@ interact_with_element <- function(list_get_elements, DEBUG = FALSE) {
 
   # MESSAGE -----------------------------------------------------------------
 
-  if (DEBUG == TRUE & length(list_get_elements$name_buttons$id) == 1 & list_get_elements$name_buttons$id == "jspsych-instructions-next") cat(crayon::bold("\n[STARTING TASK]:", gsub("\n", "", list_get_elements$name_contents$content), "\n"))
+  if (DEBUG == TRUE & length(list_get_elements$name_buttons$id) == 1 & all(list_get_elements$name_buttons$id == "jspsych-instructions-next")) cat(crayon::bold("\n[STARTING TASK]:", gsub("\n", "", list_get_elements$name_contents$content), "\n"))
 
   if (DEBUG == TRUE) cat(crayon::yellow("\n[SCREEN]:"), crayon::silver(paste0(paste(output_select_input$selected_input$name, collapse = ", "), "|", paste(selected_button_name, collapse = ", "), ":")),  content_str, crayon::yellow("[response]:"), crayon::white( paste(output_select_input$input_text_human_readable, collapse = ", ")), "\n")
 
