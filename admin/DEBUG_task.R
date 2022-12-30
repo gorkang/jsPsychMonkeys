e010d2120f88e7e4166da3ec61f55557522938f1
G <gorkang@gmail.com>
2022-12-30 12:28:26 +0000

    polish

--- admin/DEBUG_task.R
+++ admin/DEBUG_task.R
@@ -1,8 +1,8 @@
 # DEBUG TASK
 
 targets::tar_load_globals()
-debug_function("complete_task")
 targets::tar_load("parameters_monkeys")
+debug_function("complete_task")
 
 # If single participant in _targets.R
 participant_to_debug = parameters_monkeys$participants$uid
@@ -19,4 +19,6 @@ list_get_elements = get_elements(remDr = remDr, DEBUG = DEBUG); list_get_element
 # In complete_task.R
 rstudioapi::navigateToFile("R/helper_select_input.R")
 
+source("R/interact_with_element.R")
 rstudioapi::navigateToFile("R/interact_with_element.R")
+interact_with_element(list_get_elements = list_get_elements, index = 1, seed = 11)
